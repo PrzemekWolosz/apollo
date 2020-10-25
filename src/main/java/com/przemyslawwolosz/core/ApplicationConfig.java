bb1a11c6310057a33abfbdf42bf6612ed1f39fb3
PrzemekWolosz <przemyslaw.wolosz1@gmail.com>
2020-10-25 09:32:43 +0100

    Add ApplicationConfig & ApplicationConfigDto

---
+++ src/main/java/com/przemyslawwolosz/core/ApplicationConfig.java
@@ -0,0 +1,37 @@
+package com.przemyslawwolosz.core;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApplicationConfig {
+
+    @Value("${domain}") private String domain;
+    @Value("${email}") private String email;
+    @Value("${user}") private String user;
+    @Value("${theme}") private String theme;
+
+    public ApplicationConfigDto asDto() {
+        return new ApplicationConfigDto()
+                .setDomain(domain)
+                .setEmail(email)
+                .setUser(user)
+                .setTheme(theme);
+    }
+
+    public String getDomain() {
+        return domain;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public String getTheme() {
+        return theme;
+    }
+}
